bfdb7ad8cf809607949a40dac49156edbf329b88
David Joyner <david.joyner@spirent.com>
2019-05-02 15:38:31 -0400

    Initial implementation

---
+++ duration.go
@@ -0,0 +1,202 @@
+// Package duration provides an implementation of ISO8601 duration parsing and
+// formatting using time.Duration as the underlying type.
+package duration
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	// ErrBadFormat is returned when parsing fails.
+	ErrBadFormat = errors.New("bad format string")
+
+	// ErrNoMonth is returned when a month element is in the format string.
+	ErrNoMonth = errors.New("no month elements allowed")
+
+	// ErrNoNegative is returned when a negative Duration is formatted.
+	ErrNoNegative = errors.New("cannot format negative duration")
+
+	format = regexp.MustCompile(`^P((?P<year>\d+((\.|,)\d+)?)Y)?((?P<month>\d+((\.|,)\d+)?)M)?((?P<week>\d+((\.|,)\d+)?)W)?((?P<day>\d+((\.|,)\d+)?)D)?(T((?P<hour>\d+((\.|,)\d+)?)H)?((?P<minute>\d+((\.|,)\d+)?)M)?((?P<second>\d+((\.|,)\d+)?)S)?)?$`)
+)
+
+const (
+	dayTime  = 24 * time.Hour
+	weekTime = 7 * 24 * time.Hour
+	yearTime = 365 * 24 * time.Hour
+)
+
+// Parse parses an ISO8601-formatted duration value and returns a time.Duration.
+// Month elements (e.g. "P1M") are not supported.
+func Parse(s string) (time.Duration, error) {
+	match := format.FindStringSubmatch(strings.TrimSpace(s))
+	if match == nil {
+		return 0, ErrBadFormat
+	}
+
+	var d time.Duration
+	var numElems, weekElem, fracElem int
+
+	for i, name := range format.SubexpNames() {
+		part := match[i]
+		if i == 0 || name == "" || part == "" {
+			continue
+		}
+
+		whole, frac, hasFrac, err := parseDecimal(part)
+		if err != nil {
+			return 0, ErrBadFormat
+		}
+
+		// Fractional elements must be the last element in the string
+		if hasFrac {
+			if fracElem > 0 {
+				return 0, ErrBadFormat
+			}
+			fracElem = i
+		} else if fracElem > 0 {
+			return 0, ErrBadFormat
+		}
+
+		switch name {
+		case "year":
+			d += time.Duration(whole) * yearTime
+			if frac != 0 {
+				d += time.Duration(frac * float64(yearTime))
+			}
+		case "month":
+			return 0, ErrNoMonth
+		case "week":
+			d += time.Duration(whole) * weekTime
+			if frac != 0 {
+				d += time.Duration(frac * float64(weekTime))
+			}
+			weekElem = i
+		case "day":
+			d += time.Duration(whole) * dayTime
+			if frac != 0 {
+				d += time.Duration(frac * float64(dayTime))
+			}
+		case "hour":
+			d += time.Duration(whole) * time.Hour
+			if frac != 0 {
+				d += time.Duration(frac * float64(time.Hour))
+			}
+		case "minute":
+			d += time.Duration(whole) * time.Minute
+			if frac != 0 {
+				d += time.Duration(frac * float64(time.Minute))
+			}
+		case "second":
+			d += time.Duration(whole) * time.Second
+			if frac != 0 {
+				d += time.Duration(frac * float64(time.Second))
+			}
+		}
+		numElems++
+	}
+
+	// There must be at least one element in the string
+	if numElems == 0 {
+		return 0, ErrBadFormat
+	}
+
+	// Week elements, when used, must be the only elements in the string
+	if weekElem > 0 && numElems > 1 {
+		return 0, ErrBadFormat
+	}
+
+	return d, nil
+}
+
+func parseDecimal(s string) (whole int64, frac float64, hasFrac bool, err error) {
+	if sep := strings.IndexAny(s, ".,"); sep != -1 {
+		if whole, err = strconv.ParseInt(s[0:sep], 10, 64); err != nil {
+			return
+		}
+		if frac, err = strconv.ParseFloat("."+s[sep+1:], 64); err != nil {
+			return
+		}
+		hasFrac = true
+	} else {
+		whole, err = strconv.ParseInt(s, 10, 64)
+	}
+	return
+}
+
+// Format returns a string representation of a time.Duration value using ISO8601
+// formatting. Negative duration values are not supported.
+func Format(d time.Duration) (string, error) {
+	if d < 0 {
+		return "", ErrNoNegative
+	}
+
+	s := bytes.NewBufferString("P")
+	if d == 0 {
+		s.WriteString("0Y")
+		goto done
+	}
+
+	if f := d / yearTime; f >= 1 {
+		i := int(f)
+		fmt.Fprintf(s, "%dY", i)
+		d -= time.Duration(i) * yearTime
+		if d == 0 {
+			goto done
+		}
+	}
+
+	if f := d / dayTime; f >= 1 {
+		i := int(f)
+		fmt.Fprintf(s, "%dD", i)
+		d -= time.Duration(i) * dayTime
+		if d == 0 {
+			goto done
+		}
+	}
+
+	s.WriteString("T")
+
+	if f := d / time.Hour; f >= 1 {
+		i := int(f)
+		fmt.Fprintf(s, "%dH", i)
+		d -= time.Duration(i) * time.Hour
+		if d == 0 {
+			goto done
+		}
+	}
+
+	if f := d / time.Minute; f >= 1 {
+		i := int(f)
+		fmt.Fprintf(s, "%dM", i)
+		d -= time.Duration(i) * time.Minute
+		if d == 0 {
+			goto done
+		}
+	}
+
+	if d%time.Second == 0 {
+		fmt.Fprintf(s, "%dS", d/time.Second)
+		goto done
+	}
+
+	if d%time.Millisecond == 0 {
+		fmt.Fprintf(s, "%.3fS", float64(d)/float64(time.Second))
+		goto done
+	}
+
+	if d%time.Microsecond == 0 {
+		fmt.Fprintf(s, "%.6fS", float64(d)/float64(time.Second))
+		goto done
+	}
+
+	fmt.Fprintf(s, "%.9fS", float64(d)/float64(time.Second))
+
+done:
+	return s.String(), nil
+}
---
+++ duration_test.go
@@ -0,0 +1,166 @@
+package duration
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseGivenValid(t *testing.T) {
+	vecs := []struct {
+		in  string
+		out time.Duration
+	}{
+		// Full string
+		{"P1Y2DT3H4M5S", 367*24*time.Hour + 3*time.Hour + 4*time.Minute + 5*time.Second},
+
+		// Partial strings
+		{"P1Y", 365 * 24 * time.Hour},
+		{"P2W", 2 * 7 * 24 * time.Hour},
+		{"P2D", 2 * 24 * time.Hour},
+		{"PT3H", 3 * time.Hour},
+		{"PT4M", 4 * time.Minute},
+		{"PT5S", 5 * time.Second},
+
+		// Decimal fractions in smallest parts
+		{"P1.5Y", 1.5 * 365 * 24 * time.Hour},
+		{"P0.5W", 0.5 * 7 * 24 * time.Hour},
+		{"P1Y0.5D", 365*24*time.Hour + 0.5*24*time.Hour},
+		{"P1YT0.5H", 365*24*time.Hour + 0.5*60*time.Minute},
+		{"P1YT0.5M", 365*24*time.Hour + 0.5*60*time.Second},
+		{"P1YT0.5S", 365*24*time.Hour + 500*time.Millisecond},
+		{"P1.5D", 1.5 * 24 * time.Hour},
+		{"P1DT0.5H", 24*time.Hour + 0.5*60*time.Minute},
+		{"P1DT0.5M", 24*time.Hour + 0.5*60*time.Second},
+		{"P1DT0.5S", 24*time.Hour + 500*time.Millisecond},
+		{"PT1.5H", 1.5 * 60 * time.Minute},
+		{"PT1H0.5M", time.Hour + 0.5*60*time.Second},
+		{"PT1H0.5S", time.Hour + 500*time.Millisecond},
+		{"PT1.5M", 1.5 * 60 * time.Second},
+		{"PT1M0.5S", time.Minute + 500*time.Millisecond},
+		{"PT0.5S", 500 * time.Millisecond},
+	}
+
+	t.Parallel()
+
+	for _, vec := range vecs {
+		d, err := Parse(vec.in)
+		assert.NoError(t, err, vec.in)
+		assert.Equal(t, vec.out, d, vec.in)
+	}
+}
+
+func TestParseGivenInvalid(t *testing.T) {
+	vecs := []struct {
+		in  string
+		err error
+	}{
+		// Bad formats
+		{"", ErrBadFormat},
+		{"asdf", ErrBadFormat},
+		{"P", ErrBadFormat},
+		{"P1", ErrBadFormat},
+		{"P1X", ErrBadFormat},
+		{"P1y", ErrBadFormat},
+		{"1Y", ErrBadFormat},
+		{"P5S1Y", ErrBadFormat},
+		{"P1.0Y5S", ErrBadFormat},
+		{"P1.0YT5S", ErrBadFormat},
+		{"P1.0YT5.0S", ErrBadFormat},
+		{"P1Y2W3D4H6M6S", ErrBadFormat},
+		{"P1Y1W", ErrBadFormat},
+		{"P1S", ErrBadFormat},
+
+		// With month
+		{"P0M", ErrNoMonth},
+		{"P1M", ErrNoMonth},
+		{"P1Y1M", ErrNoMonth},
+		{"P0MT1M", ErrNoMonth},
+		{"P1MT1M", ErrNoMonth},
+	}
+
+	t.Parallel()
+
+	for _, vec := range vecs {
+		d, err := Parse(vec.in)
+		if assert.Error(t, err, vec.in) {
+			assert.Equal(t, vec.err, err, vec.in)
+		}
+		assert.Equal(t, time.Duration(0), d, vec.in)
+	}
+}
+
+func TestFormatGivenValid(t *testing.T) {
+	t.Parallel()
+
+	vecs := []struct {
+		in  time.Duration
+		out string
+	}{
+		// Zero-value
+		{time.Duration(0), "P0Y"},
+
+		// Smaller than a second
+		{time.Nanosecond, "PT0.000000001S"},
+		{time.Microsecond, "PT0.000001S"},
+		{time.Millisecond, "PT0.001S"},
+
+		// Smaller than a minute
+		{time.Second, "PT1S"},
+		{time.Second + time.Millisecond, "PT1.001S"},
+		{59 * time.Second, "PT59S"},
+		{59*time.Second + time.Millisecond, "PT59.001S"},
+
+		// Smaller than an hour
+		{time.Minute, "PT1M"},
+		{time.Minute + time.Second, "PT1M1S"},
+		{time.Minute + time.Second + time.Millisecond, "PT1M1.001S"},
+
+		// Smaller than a day
+		{time.Hour, "PT1H"},
+		{time.Hour + time.Minute, "PT1H1M"},
+		{time.Hour + time.Minute + time.Second, "PT1H1M1S"},
+		{time.Hour + time.Minute + time.Second + time.Millisecond, "PT1H1M1.001S"},
+
+		// Smaller than a week (NB: should not produce week-based values)
+		{24*time.Hour + time.Hour, "P1DT1H"},
+
+		// Smaller than a year
+		{10*24*time.Hour + time.Hour, "P10DT1H"},
+		{10*24*time.Hour + time.Hour + time.Minute, "P10DT1H1M"},
+		{10*24*time.Hour + time.Hour + time.Minute + time.Second, "P10DT1H1M1S"},
+		{10*24*time.Hour + time.Hour + time.Minute + time.Second + time.Millisecond, "P10DT1H1M1.001S"},
+
+		// Larger than year
+		{366*24*time.Hour + time.Hour + time.Minute + time.Second + time.Millisecond, "P1Y1DT1H1M1.001S"},
+		{375*24*time.Hour + time.Hour + time.Minute + time.Second + time.Millisecond, "P1Y10DT1H1M1.001S"},
+	}
+
+	for _, vec := range vecs {
+		s, err := Format(vec.in)
+		assert.NoError(t, err, "%s", vec.in)
+		assert.Equal(t, vec.out, s, "%s", vec.in)
+	}
+}
+
+func TestFormatGivenInvalid(t *testing.T) {
+	t.Parallel()
+
+	vecs := []struct {
+		in  time.Duration
+		err error
+	}{
+		// Negative durations
+		{-1 * time.Millisecond, ErrNoNegative},
+		{-1 * time.Second, ErrNoNegative},
+	}
+
+	for _, vec := range vecs {
+		s, err := Format(vec.in)
+		if assert.Error(t, err, "%s", vec.in) {
+			assert.Equal(t, vec.err, err, "%s", vec.in)
+		}
+		assert.Empty(t, s, "%s", vec.in)
+	}
+}
